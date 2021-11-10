950b4e4f94e10251bb43974a99cce4114f0e15a4
Sonya-Shultz <sonya.shu2107@gmail.com>
2021-11-10 13:11:07 +0200

    lab3 rewrote relations

--- services/service2/main.go
+++ services/service2/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
+
+	kafka "github.com/segmentio/kafka-go"
 )
 
 func main() {
+	ctx := context.Background()
+	go consume(ctx)
 	var status = "OK"
 
-	http.HandleFunc("/api/service2/untested-request", func(w http.ResponseWriter, r *http.Request) {
-		status = "FAILED"
-		fmt.Fprintf(w, "Service2 successfully broken!")
-	})
+	//http.HandleFunc("/api/service2/untested-request", func(w http.ResponseWriter, r *http.Request) {
+	//	status = "FAILED"
+	//	fmt.Fprintf(w, "Service2 successfully broken!")
+	//})
 	http.HandleFunc("/api/service2", func(w http.ResponseWriter, r *http.Request) {
 		if status != "OK" {
 			time.Sleep(10 * time.Second)
@@ -20,4 +25,24 @@ func main() {
 		fmt.Fprintf(w, "Hello from go server")
 	})
 	http.ListenAndServe(":8080", nil)
+}
+
+const (
+	topic         = "lab3_messages"
+	brokerAddress = "kafka:9092"
+)
+
+func consume(ctx context.Context) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+		GroupID: "service2",
+	})
+	for {
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			panic("Can't read message " + err.Error())
+		}
+		fmt.Println("we get by service2:", string(msg.Value))
+	}
 }
