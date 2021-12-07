8d03219cf7b09ac6ef62676a47a3cfc3b4776e0b
DezertRose <57520792+DezertRose@users.noreply.github.com>
2021-12-07 20:26:04 +0200

    Update docker.sh
    some instractions

--- docker.sh
+++ docker.sh
@@ -6,3 +6,15 @@ docker build -t client:0.1 -f client/Dockerfile .
 docker build -t service1:0.2 -f services/service1/Dockerfile .
 docker build -t service2:0.2 -f services/service2/Dockerfile .
 docker build -t root-service:0.1 -f services/root-service/Dockerfile .
+
+
+1. minikube delete (почистить місце)
+2. minikube start --driver=hyperv --cpus 3 --memory 4096 (мінять кількість пам'ять і тд, бо ми слабі)
+3. docker build -t service1:0.3 -f services/service1/Dockerfile . (одразу 3я версія, бо пропускаєм всі лаби і йдем до кінця)
+4. docker build -t service2:0.3 -f services/service2/Dockerfile .
+5. docker build -t root-service:0.3 -f services/root-service/Dockerfile .
+6. kubectl apply -f k8s_v4 (білд без istio)
+7. kubectl apply -f k8s (білд ingress для зручного рероуту)
+8. kubectl apply -f k8s/postgres
+9. kubectl apply -f k8s/storage
+10. kubectl apply -f k8s/kafka
